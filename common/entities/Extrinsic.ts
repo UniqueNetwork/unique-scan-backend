8d88d12cfc9ede0547ba070cf852863906318100
azgatg <azgatherzjob@gmail.com>
2022-05-19 21:27:37 +0300

    actualize collections
    -remove useless collections

--- common/entities/Extrinsic.ts
+++ common/entities/Extrinsic.ts
@@ -1,10 +1,12 @@
 import { Column, Entity, Index } from 'typeorm';
 
-@Index('extrinsic_args_idx', ['args'], {})
 @Index('extrinsic_pkey', ['block_number', 'extrinsic_index'], { unique: true })
+@Index('extrinsic_block_index_idx', ['block_index'], { unique: true })
 @Index('extrinsic_method_idx', ['method'], {})
 @Index('extrinsic_section_idx', ['section'], {})
 @Index('extrinsic_signer_idx', ['signer'], {})
+@Index('extrinsic_timestamp_idx', ['timestamp'], {})
+@Index('extrinsic_to_owner_idx', ['to_owner'], {})
 @Entity('extrinsic', { schema: 'public' })
 export class Extrinsic {
   @Column('bigint', { primary: true, name: 'block_number' })
@@ -51,4 +53,10 @@ export class Extrinsic {
 
   @Column({ length: 255, nullable: true })
   to_owner?: string;
+
+  @Column('text', { name: 'signer_normalized', nullable: true })
+  signer_normalized: string | null;
+
+  @Column('text', { name: 'to_owner_normalized', nullable: true })
+  to_owner_normalized?: string | null;
 }
