231afa569102f44b05aec8a7fdaf4dd760585331
degtiv <degtiv@yandex.ru>
2022-06-07 13:36:04 +0300

    add playground scripts

---
+++ tests/utils/unique/logger.js
@@ -0,0 +1,163 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable no-console */
+/* eslint-disable no-prototype-builtins */
+
+const addLeadZero = (num) => {
+  if (num < 10) {
+    return `0${num}`;
+  }
+  return `${num}`;
+};
+
+class Logger {
+  static FORMATS = {
+    cmd: {
+      reset: 0,
+      bright: 1,
+      dim: 2,
+      underscore: 4,
+      blink: 5,
+      reverse: 7,
+      hidden: 8,
+    },
+    fg: {
+      black: 30,
+      red: 31,
+      green: 32,
+      yellow: 33,
+      blue: 34,
+      magenta: 35,
+      cyan: 36,
+      white: 37,
+      crimson: 38,
+    },
+    bg: {
+      black: 40,
+      red: 41,
+      green: 42,
+      yellow: 43,
+      blue: 44,
+      magenta: 45,
+      cyan: 46,
+      white: 47,
+      crimson: 48,
+    },
+  };
+
+  static LEVEL = {
+    ERROR: 'ERROR',
+    WARNING: 'WARNING',
+    INFO: 'INFO',
+    DEBUG: 'DEBUG',
+    NONE: 'NONE',
+  };
+
+  static LEVEL_COLOR = {
+    ERROR: 'fg.red',
+    WARNING: 'fg.yellow',
+    INFO: 'fg.green',
+  };
+
+  static getFmt(fmt) {
+    let format = this.FORMATS;
+    for (const part of fmt.split('.')) {
+      format = typeof format !== 'undefined' ? format[part] : format;
+    }
+    if (typeof format !== 'number') {
+      format = this.FORMATS.cmd.reset;
+    }
+    return format;
+  }
+
+  static getFmtSymbol(code) {
+    return `\x1b[${code}m`;
+  }
+
+  static getTimestamp() {
+    const d = new Date();
+    const year = d.getFullYear();
+    const month = addLeadZero(d.getMonth() + 1);
+    const date = addLeadZero(d.getDate());
+    const hour = addLeadZero(d.getHours());
+    const min = addLeadZero(d.getMinutes());
+    const sec = addLeadZero(d.getSeconds());
+    return `${year}-${month}-${date} ${hour}:${min}:${sec}`;
+  }
+
+  static formatString(str, fmt) {
+    const fmtSymbol = this.getFmtSymbol(
+      this.getFmt(typeof fmt === 'undefined' ? '' : fmt),
+    );
+    return `${fmtSymbol}${str}${this.getFmtSymbol(this.FORMATS.cmd.reset)}`;
+  }
+
+  constructor(includeTime = true, defaultLevel = Logger.LEVEL.INFO) {
+    this.includeTime = includeTime;
+    this.defaultLevel = defaultLevel;
+    this.level = JSON.parse(JSON.stringify(this.constructor.LEVEL));
+  }
+
+  fmtSymbol(fmt) {
+    return this.constructor.getFmtSymbol(this.constructor.getFmt(fmt));
+  }
+
+  fmt(str, fmt) {
+    return this.constructor.formatString(str, fmt);
+  }
+
+  write(...messages) {
+    console.log(...messages);
+  }
+
+  constructLog(message, level) {
+    if (typeof level === 'undefined') {
+      level = this.defaultLevel;
+    }
+    if (level === this.level.ERROR) {
+      message = message?.stack || message;
+    }
+
+    const rawMsgs = Array.isArray(message) ? message : [message];
+    let msgs =
+      level !== this.level.NONE
+        ? [
+            `${
+              this.constructor.LEVEL_COLOR.hasOwnProperty(level)
+                ? this.fmt(level, this.constructor.LEVEL_COLOR[level])
+                : level
+            }:`,
+          ]
+        : [];
+    for (const msg of rawMsgs) {
+      try {
+        if (typeof msg !== 'string') {
+          msgs.push(JSON.stringify(msg));
+        } else {
+          msgs.push(msg);
+        }
+      } catch (e) {
+        console.error(this.fmtSymbol('fg.red'), e, this.fmtSymbol('cmd.reset'));
+      }
+    }
+    if (this.includeTime) {
+      msgs = [
+        `[${this.fmt(this.constructor.getTimestamp(), 'fg.cyan')}]`,
+        ...msgs,
+      ];
+    }
+    return msgs;
+  }
+
+  log(message, level) {
+    this.write(...this.constructLog(message, level));
+  }
+}
+
+class SilentLogger extends Logger {
+  write(...messages) {}
+}
+
+module.exports = {
+  Logger,
+  SilentLogger,
+};
