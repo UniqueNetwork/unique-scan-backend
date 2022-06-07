aeea3f273d6a0be68d164114c21149bfdc02f36f
degtiv <degtiv@yandex.ru>
2022-06-07 16:31:18 +0300

    fixing local environment startup scripts for testing

--- tests/scripts/run-chain.sh
+++ tests/scripts/run-chain.sh
@@ -3,5 +3,5 @@
 ENV_FILE=${1}
 export $(grep -v '^#' $ENV_FILE | xargs)
 
-git clone -b $CHAIN_VERSION git@gitlab.uniquenetwork.dev:qa-team/run-quartz.git ./application/chain
-docker-compose -f application/chain/docker-compose.yml up -d --build
+git clone -b $TESTS_CHAIN_VERSION git@gitlab.uniquenetwork.dev:qa-team/run-quartz.git ../application/chain
+docker-compose -f ../application/chain/docker-compose.yml up -d --build
