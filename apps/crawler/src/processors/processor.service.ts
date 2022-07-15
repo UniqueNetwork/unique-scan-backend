d9a8598466a2b97eafda5efda6bced236c2b98c4
Sergey Chernetsky <chernetsky@gmail.com>
2022-07-15 10:58:16 +0800

    wip

---
+++ apps/crawler/src/processors/processor.service.ts
@@ -0,0 +1,7 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { SubstrateProcessor } from '@subsquid/substrate-processor'
+
+@Injectable()
+export class ProcessorService {
+    processor: 
+}
