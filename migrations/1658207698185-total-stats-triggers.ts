2b0e2bede11ff5a085231871e8007be5cfa310f0
azgatg <azgatherzjob@gmail.com>
2022-07-19 13:47:10 +0300

    total statistic triggers
    -migration for triggers
    -fix dto

---
+++ migrations/1658207698185-total-stats-triggers.ts
@@ -0,0 +1,357 @@
+/* eslint-disable max-len */
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { Total } from '../common/entities/Total';
+
+export class totalStatsTriggers1658207698185 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.query(`drop trigger if exists update_total on total;`);
+      await queryRunner.query('drop trigger if exists blocks_total ON block;');
+      await queryRunner.query(
+        `drop trigger if exists extrinsic_total on extrinsic;`,
+      );
+      await queryRunner.query(`drop trigger if exists events_total on event;`);
+      await queryRunner.query(
+        `drop trigger if exists collections_total on collections;`,
+      );
+      await queryRunner.query(`drop trigger if exists tokens_total on tokens;`);
+      await queryRunner.query(
+        `drop trigger if exists transfers_total on extrinsic;`,
+      );
+      await queryRunner.query(
+        `drop trigger if exists update_supply on account;`,
+      );
+
+      await queryRunner.query(
+        `UPDATE total SET count = (SELECT count(*) FROM block) WHERE name = 'blocks';`,
+      );
+
+      await queryRunner.query(
+        `
+        create or replace function update_blocks_total() returns trigger as $$
+        begin
+        if  (TG_OP = 'INSERT') then
+        update total set count = count + 1 where name = 'blocks';
+        end if;
+
+        if  (TG_OP = 'DELETE') then
+        update total set count = count - 1 where name = 'blocks';
+        end if;
+
+        return null;
+        end;
+        $$ LANGUAGE plpgsql;
+        `,
+      );
+
+      await queryRunner.query(
+        `
+        create trigger blocks_total AFTER INSERT OR DELETE ON block
+        FOR EACH ROW
+        EXECUTE FUNCTION update_blocks_total();
+        `,
+      );
+
+      await queryRunner.query(
+        `UPDATE total SET count = (SELECT count(*) FROM extrinsic) WHERE name = 'extrinsics';`,
+      );
+
+      await queryRunner.query(
+        `
+        create or replace function update_extrinsics_total() returns trigger as $$
+        begin
+        if  (TG_OP = 'INSERT') then
+        update total set count = count + 1 where name = 'extrinsics';
+        end if;
+
+        if  (TG_OP = 'DELETE') then
+        update total set count = count - 1 where name = 'extrinsics';
+        end if;
+
+        return null;
+        end;
+        $$ LANGUAGE plpgsql;
+        `,
+      );
+
+      await queryRunner.query(
+        `
+        create trigger extrinsic_total after insert or delete on extrinsic
+        FOR EACH row
+        execute function update_extrinsics_total();
+        `,
+      );
+
+      await queryRunner.query(
+        `UPDATE total SET count = (SELECT count(*) FROM event) WHERE name = 'events';`,
+      );
+
+      await queryRunner.query(
+        `
+        create or replace function update_events_total() returns trigger as $$
+        begin
+        if  (TG_OP = 'INSERT') then
+        update total set count = count + 1 where name = 'events';
+        end if;
+
+        if  (TG_OP = 'DELETE') then
+        update total set count = count - 1 where name = 'events';
+        end if;
+
+        return null;
+        end;
+        $$ LANGUAGE plpgsql;
+        `,
+      );
+
+      await queryRunner.query(
+        `
+        create trigger events_total after insert or delete on event
+        FOR EACH row
+        execute function update_events_total();
+        `,
+      );
+
+      await queryRunner.query(
+        `UPDATE total SET count = (SELECT count(*) FROM collections) WHERE name ='collections';`,
+      );
+
+      await queryRunner.query(
+        `
+        create or replace function update_collections_total() returns trigger as $$
+        begin
+        if  (TG_OP = 'INSERT') then
+          update total set count = count + 1 where name = 'collections';
+        end if;
+
+        if  (TG_OP = 'DELETE') then
+          update total set count = count - 1 where name = 'collections';
+        end if;
+
+        return null;
+        end;
+        $$ LANGUAGE plpgsql;
+        `,
+      );
+
+      await queryRunner.query(
+        `
+        create trigger collections_total after insert or delete on collections
+        FOR EACH row
+        execute function update_collections_total();
+        `,
+      );
+
+      await queryRunner.query(
+        `UPDATE total SET count = (SELECT count(*) FROM tokens) WHERE name ='tokens';`,
+      );
+
+      await queryRunner.query(
+        `
+        create or replace function update_tokens_total() returns trigger as $$
+        begin
+        if  (TG_OP = 'INSERT') then
+          update total set count = count + 1 where name = 'tokens';
+        end if;
+
+        if  (TG_OP = 'DELETE') then
+          update total set count = count - 1 where name = 'tokens';
+        end if;
+
+        return null;
+        end;
+        $$ LANGUAGE plpgsql;
+        `,
+      );
+
+      await queryRunner.query(
+        `
+        create trigger tokens_total after insert or delete on tokens
+        FOR EACH row
+        execute function update_tokens_total();
+        `,
+      );
+
+      await queryRunner.query(
+        `UPDATE total SET count = (SELECT count(*) FROM extrinsic WHERE section = 'balances' and method = 'transfer' ) WHERE name = 'transfers';`,
+      );
+
+      await queryRunner.query(
+        `
+        create or replace function update_transfers_total() returns trigger as $$
+        begin
+        if  (TG_OP = 'INSERT' and new.section = 'balances' and new.method = 'transfer') then
+          update total set count = count + 1 where name = 'transfers';
+        end if;
+
+        if  (TG_OP = 'DELETE' and old.section = 'balances' and old.method = 'transfer') then
+          update total set count = count - 1 where name = 'transfers';
+        end if;
+
+        return null;
+        end;
+        $$ LANGUAGE plpgsql;
+        `,
+      );
+
+      await queryRunner.query(
+        `
+        create trigger transfers_total after insert or delete on extrinsic
+        FOR EACH row
+        execute function update_transfers_total();
+        `,
+      );
+
+      // accounts stats
+      const stat = await queryRunner.query(
+        `
+          select
+            coalesce(sum(available_balance::double precision), 0) as locked,
+            coalesce(sum(locked_balance::double precision), 0) as circulating,
+            coalesce(sum(available_balance::double precision + locked_balance::double precision), 0) as total,
+            count(*) filter (where available_balance <> '0' or locked_balance <> '0') as holders
+          from account a
+        `,
+      );
+      const { locked, circulating, total, holders } = stat[0];
+
+      if (locked) {
+        await queryRunner.manager.upsert(
+          Total,
+          { count: Math.floor(locked), name: 'locked_supply' },
+          ['name'],
+        );
+      }
+      if (circulating) {
+        await queryRunner.manager.upsert(
+          Total,
+          { count: Math.floor(circulating), name: 'circulating_supply' },
+          ['name'],
+        );
+      }
+
+      if (total) {
+        await queryRunner.manager.upsert(
+          Total,
+          { count: Math.floor(total), name: 'total_supply' },
+          ['name'],
+        );
+      }
+
+      if (holders) {
+        await queryRunner.manager.upsert(
+          Total,
+          { count: holders, name: 'holders' },
+          ['name'],
+        );
+      }
+
+      await queryRunner.query(
+        `
+        create or replace function update_account_supply() returns trigger as $$
+        begin
+        if  (TG_OP = 'INSERT' and new.available_balance <> '0') then
+          update total set count = count + new.available_balance::double precision where name = 'locked_supply';
+        end if;
+
+        if  (TG_OP = 'UPDATE' and new.available_balance <> old.available_balance) then
+          update total set count = count + new.available_balance::double precision - old.available_balance::double precision
+           where name = 'locked_supply';
+        end if;
+
+        if  (TG_OP = 'INSERT' and new.locked_balance <> '0') then
+          update total set count = count + new.locked_balance::double precision where name = 'circulating_supply';
+        end if;
+
+        if  (TG_OP = 'UPDATE' and new.locked_balance <> old.locked_balance) then
+          update total set count = count + new.locked_balance::double precision - old.locked_balance::double precision
+           where name = 'circulating_supply';
+        end if;
+
+        if  (TG_OP = 'INSERT' and new.locked_balance <> '0' and new.available_balance <> '0') then
+          update total set count = count + 1 where name = 'holders';
+        end if;
+
+        if  (TG_OP = 'UPDATE' and new.locked_balance = '0' and new.available_balance = '0') then
+          update total set count = count - 1 where name = 'holders';
+        end if;
+
+        if  (TG_OP = 'UPDATE' and ((old.locked_balance = '0' and new.locked_balance <> '0') or (old.available_balance = '0' and new.available_balance <> '0'))) then
+          update total set count = count + 1 where name = 'holders';
+        end if;
+
+        return null;
+        end;
+        $$ LANGUAGE plpgsql;
+        `,
+      );
+
+      await queryRunner.query(
+        `
+        create trigger update_supply AFTER INSERT OR DELETE ON account
+        FOR EACH ROW
+        EXECUTE FUNCTION update_account_supply();
+        `,
+      );
+
+      await queryRunner.query(
+        `
+        create or replace function update_total_supply() returns trigger as $$
+        begin
+        if  (TG_OP = 'INSERT' and (new.locked_supply <> 0 or new.circulating_supply <> 0)) then
+          update total set count = count + coalesce(new.locked_supply, 0) + coalesce(new.circulating_supply, 0) where name = 'total_supply';
+        end if;
+
+        if  (TG_OP = 'UPDATE') then
+          update total set count = count + coalesce(new.locked_supply, 0) + coalesce(new.circulating_supply, 0) where name = 'total_supply';
+        end if;
+
+        return null;
+        end;
+        $$ LANGUAGE plpgsql;
+        `,
+      );
+
+      await queryRunner.query(
+        `
+        create trigger update_total AFTER INSERT OR DELETE ON total
+        FOR EACH ROW
+        EXECUTE FUNCTION update_total_supply();
+        `,
+      );
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`drop trigger blocks_total on block;`);
+    await queryRunner.query(`drop function update_blocks_total;`);
+
+    await queryRunner.query(`drop trigger extrinsic_total on extrinsic;`);
+    await queryRunner.query(`drop function update_extrinsics_total;`);
+
+    await queryRunner.query(`drop trigger events_total on event;`);
+    await queryRunner.query(`drop function update_events_total;`);
+
+    await queryRunner.query(`drop trigger collections_total on collections;`);
+    await queryRunner.query(`drop function update_collections_total;`);
+
+    await queryRunner.query(`drop trigger tokens_total on tokens;`);
+    await queryRunner.query(`drop function update_tokens_total;`);
+
+    await queryRunner.query(`drop trigger transfers_total on extrinsic;`);
+    await queryRunner.query(`drop function update_transfers_total;`);
+
+    await queryRunner.query(`drop trigger update_supply on extrinsic;`);
+    await queryRunner.query(`drop function update_account_supply0;`);
+
+    await queryRunner.query(`drop trigger update_total on extrinsic;`);
+    await queryRunner.query(`drop function update_total_supply;`);
+  }
+}
