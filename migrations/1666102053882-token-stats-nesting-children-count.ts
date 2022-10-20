57a24279bc6cb1ff15be2d060fb71d493a0193f6
AzgatG <azgatherzjob@gmail.com>
2022-10-20 16:44:07 +0300

    fix migration

--- migrations/1666102053882-token-stats-nesting-children-count.ts
+++ migrations/1666102053882-token-stats-nesting-children-count.ts
@@ -7,9 +7,6 @@ export class tokenStatsNestingChildrenCount1666102053882
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "tokens" ADD "bundle_created" bigint`);
-    await queryRunner.query(
-      `CREATE INDEX "tokens_parent_id_idx" ON "tokens" ("parent_id") `,
-    );
 
     await queryRunner.query(
       `ALTER TABLE "tokens_stats" ADD "children_count" bigint`,
@@ -17,7 +14,6 @@ export class tokenStatsNestingChildrenCount1666102053882
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "public"."tokens_parent_id_idx"`);
     await queryRunner.query(
       `ALTER TABLE "tokens" DROP COLUMN "bundle_created"`,
     );
