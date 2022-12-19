283b93f1069e9866c6d2ae44e35cc4f0eb960521
ashkuc <ashkucc@gmail.com>
2022-12-19 09:18:49 +0100

    Add harvester

--- apps/crawler/src/crawler.module.ts
+++ apps/crawler/src/crawler.module.ts
@@ -8,11 +8,28 @@ import { SubscribersModule } from './subscribers/subscribers.module';
 import { Config, GlobalConfigModule } from './config/config.module';
 import { CacheProviderModule } from './cache/cache-provider.module';
 
+import { HarvesterModule, HarvesterModuleOptions } from '@ashkuc/harvester';
+
+const harvester = HarvesterModule.registerAsync({
+  useFactory: () =>
+    ({
+      chainWsUrl: process.env.CHAIN_WS_URL,
+      database: {
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+      },
+    } as HarvesterModuleOptions),
+});
+
 @Module({
   imports: [
     GlobalConfigModule,
     CacheProviderModule,
     TypeOrmModule.forRoot(typeormConfig),
+    harvester,
     SentryModule.forRootAsync({
       useFactory: async (configService: ConfigService<Config>) => {
         return configService.get('sentry');
