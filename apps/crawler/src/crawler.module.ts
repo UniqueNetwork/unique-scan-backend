bf782a22fbf995ef86e729ad57042e46f21ddf62
ashkuc <ashkucc@gmail.com>
2022-12-19 15:20:15 +0100

    Reuse typeormConfig

--- apps/crawler/src/crawler.module.ts
+++ apps/crawler/src/crawler.module.ts
@@ -10,26 +10,19 @@ import { CacheProviderModule } from './cache/cache-provider.module';
 
 import { HarvesterModule, HarvesterModuleOptions } from '@ashkuc/harvester';
 
-const harvester = HarvesterModule.registerAsync({
-  useFactory: () =>
-    ({
-      chainWsUrl: process.env.CHAIN_WS_URL,
-      database: {
-        host: process.env.POSTGRES_HOST,
-        port: +process.env.POSTGRES_PORT,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DATABASE,
-      },
-    } as HarvesterModuleOptions),
-});
-
 @Module({
   imports: [
     GlobalConfigModule,
     CacheProviderModule,
     TypeOrmModule.forRoot(typeormConfig),
-    harvester,
+    HarvesterModule.registerAsync({
+      useFactory: (config: ConfigService<Config>) =>
+        ({
+          chainWsUrl: config.get('chainWsUrl'),
+          database: typeormConfig,
+        } as HarvesterModuleOptions),
+      inject: [ConfigService],
+    }),
     SentryModule.forRootAsync({
       useFactory: async (configService: ConfigService<Config>) => {
         return configService.get('sentry');
