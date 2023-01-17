43cb9392a40fa6a9c83f65916de2210f51f7559a
icehuntmen <icehuntmen@gmail.com>
2023-01-17 15:10:04 +0300

    Update migration tokens nested data

--- migrations/1673954094424-update-nested.ts
+++ migrations/1673954094424-update-nested.ts
@@ -7,20 +7,6 @@ export class updatenested1673954094424 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "tokens" ADD IF NOT EXISTS "nested" boolean NOT NULL DEFAULT false`,
     );
-    try {
-      await queryRunner.query(
-        `UPDATE tokens SET nested = true WHERE type = "NESTED"`,
-      );
-      await queryRunner.query(
-        `UPDATE tokens SET type = "NFT" WHERE type = "NESTED" AND total_pieces = 1`,
-      );
-      await queryRunner.query(
-        `UPDATE tokens SET type = "RFT" WHERE type = "NESTED" AND total_pieces > 1`,
-      );
-    } catch (e) {
-      await queryRunner.rollbackTransaction();
-      throw e;
-    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
