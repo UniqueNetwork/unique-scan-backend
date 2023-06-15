c8444aeaf9add48f5dff42f02bd472d5832a43ae
pasynkov <n@pasynkov.com>
2023-06-15 15:42:04 +0300

    fix amounts

--- apps/crawler/src/services/event/event.types.ts
+++ apps/crawler/src/services/event/event.types.ts
@@ -21,4 +21,7 @@ export type EventValues = {
   to?: {
     value: string;
   };
+  who?: {
+    value: string;
+  };
 } & { [key in AccountKey]: string };
--- apps/crawler/src/services/extrinsic.service.ts
+++ apps/crawler/src/services/extrinsic.service.ts
@@ -10,8 +10,10 @@ import { EventValues } from './event/event.types';
 
 import { TokensOwners } from '@entities/TokensOwners';
 
-import { ExtrinsicEntity } from '@unique-nft/harvester/src/database/entities';
+import { EventEntity, ExtrinsicEntity } from '@unique-nft/harvester/src/database/entities';
 import { SdkService } from '@common/sdk/sdk.service';
+import { EventService } from './event/event.service';
+import { EventArgumentsService } from './event/event.arguments.service';
 
 const EXTRINSICS_TRANSFER_METHODS = [
   ExtrinsicMethod.TRANSFER,
@@ -41,10 +43,11 @@ export class ExtrinsicService {
     @InjectRepository(TokensOwners)
     private tokensOwnersRepository: Repository<TokensOwners>,
     private sdkService: SdkService,
+    private eventArgumentsService: EventArgumentsService,
   ) {}
 
-  upsert(extrinsicsEntity: ExtrinsicEntity[]) {
-    const extrinsicsData = this.prepareDataForDbNew(extrinsicsEntity);
+  async upsert(extrinsicsEntity: ExtrinsicEntity[]) {
+    const extrinsicsData = await this.prepareDataForDbNew(extrinsicsEntity);
 
     return this.extrinsicsRepository.upsert(extrinsicsData, [
       'block_number',
@@ -55,91 +58,87 @@ export class ExtrinsicService {
   /**
    * Extracts amount and fee values from extrinsic's events.
    */
-  private getAmountValues(blockIndex: string, eventsData: Event[]) {
-    return eventsData
-      .filter(
-        ({ block_index: eventBlockIndex }) => eventBlockIndex === blockIndex,
-      )
-      .reduce(
-        (
-          acc: { amount: string; fee: string; toOwner: string | null },
-          curr: Event,
-        ) => {
-          const { section, method } = curr;
-          const values = curr.values as unknown as EventValues;
+  private async getAmountValues(blockIndex: string, eventsData: EventEntity[]): Promise<any> {
 
-          if (!values?.amount) {
-            return acc;
-          }
+    const acc = { amount: '0', fee: '0', toOwner: null };
 
-          if (
-            section === EventSection.BALANCES &&
-            method == EventMethod.TRANSFER
-          ) {
-            acc.amount = values.amount;
-          } else if (
-            section === EventSection.TREASURY &&
-            method == EventMethod.DEPOSIT
-          ) {
-            acc.fee = values.amount;
-          } else if (
-            (section === EventSection.COMMON ||
-              section === EventSection.UNIQUE) &&
-            method === EventMethod.TRANSFER
-          ) {
-            acc.toOwner = values.to?.value || null;
-          }
+    for (const curr of eventsData) {
+      const { section, method, dataJson } = curr;
 
-          return { ...acc };
-        },
-        { amount: '0', fee: '0', toOwner: null },
+      const eventName = `${capitalize(section)}.${capitalize(method)}`;
+
+      const { values } = await this.eventArgumentsService.eventDataConverter(
+        dataJson,
+        eventName,
       );
+
+      if (
+        section.toLowerCase() === EventSection.BALANCES.toLowerCase() &&
+        method.toLowerCase() === EventMethod.TRANSFER.toLowerCase()
+      ) {
+        acc.amount = values.amount;
+        acc.toOwner = values.to?.value || null;
+      } else if (
+        section.toLowerCase() === EventSection.TREASURY.toLowerCase() &&
+        method.toLowerCase() === EventMethod.DEPOSIT.toLowerCase()
+      ) {
+        acc.fee = values.amount;
+      } else if (
+        (section.toLowerCase() === EventSection.COMMON.toLowerCase() ||
+          section.toLowerCase() === EventSection.UNIQUE.toLowerCase() ) &&
+        method.toLowerCase() === EventMethod.TRANSFER.toLowerCase()
+      ) {
+        acc.toOwner = values.to?.value || null;
+      }
+    }
+    return acc;
   }
 
-  private prepareDataForDbNew(extrinsicItems): Extrinsic[] {
-    return extrinsicItems.map((extrinsic) => {
-      const { signer, index, events } = extrinsic;
-      const blockIndex = `${extrinsic.block.id}-${index}`;
-      const amountValues = this.getAmountValues(blockIndex, events);
-      const section = capitalize(extrinsic.section);
-      const method = capitalize(extrinsic.method);
+  private async prepareDataForDbNew(extrinsicItems): Promise<Extrinsic[]> {
+    return Promise.all(
+      extrinsicItems.map(async (extrinsic) => {
+        const { signer, index, events } = extrinsic;
+        const blockIndex = `${extrinsic.block.id}-${index}`;
+        const amountValues = await this.getAmountValues(blockIndex, events);
+        const section = capitalize(extrinsic.section);
+        const method = capitalize(extrinsic.method);
 
-      // Don't need to use AccountService for signer and to_owner addresses,
-      // // because all addresses are already processed in EventService.
-      //
-      // let toOwner = null;
-      // if (EXTRINSICS_TRANSFER_METHODS.includes(method)) {
-      //   const recipientAddress = args as IExtrinsicRecipient;
-      //   const rawToOwner =
-      //     recipientAddress?.recipient?.value || recipientAddress?.dest?.value;
-      //   toOwner = normalizeSubstrateAddress(rawToOwner, ss58Prefix);
-      // }
+        // Don't need to use AccountService for signer and to_owner addresses,
+        // // because all addresses are already processed in EventService.
+        //
+        // let toOwner = null;
+        // if (EXTRINSICS_TRANSFER_METHODS.includes(method)) {
+        //   const recipientAddress = args as IExtrinsicRecipient;
+        //   const rawToOwner =
+        //     recipientAddress?.recipient?.value || recipientAddress?.dest?.value;
+        //   toOwner = normalizeSubstrateAddress(rawToOwner, ss58Prefix);
+        // }
 
-      //
-      const { amount, fee, toOwner } = amountValues;
+        //
+        const { amount, fee, toOwner } = amountValues;
 
-      return {
-        timestamp: String(
-          normalizeTimestamp(new Date(extrinsic.block.timestamp).getTime()),
-        ),
-        block_number: String(extrinsic.block.id),
-        block_index: blockIndex,
-        extrinsic_index: extrinsic.index,
-        section,
-        method,
-        hash: extrinsic.block.hash,
-        success: !!signer,
-        is_signed: !!signer,
-        signer,
-        signer_normalized: signer,
-        to_owner: toOwner,
-        to_owner_normalized: toOwner
-          ? normalizeSubstrateAddress(toOwner)
-          : null,
-        amount,
-        fee,
-      };
-    });
+        return {
+          timestamp: String(
+            normalizeTimestamp(new Date(extrinsic.block.timestamp).getTime()),
+          ),
+          block_number: String(extrinsic.block.id),
+          block_index: blockIndex,
+          extrinsic_index: index,
+          section,
+          method,
+          hash: extrinsic.block.hash,
+          success: !!signer,
+          is_signed: !!signer,
+          signer,
+          signer_normalized: signer,
+          to_owner: toOwner,
+          to_owner_normalized: toOwner
+            ? normalizeSubstrateAddress(toOwner)
+            : null,
+          amount,
+          fee,
+        };
+      }));
   }
   // private prepareDataForDb({
   //   blockCommonData,
