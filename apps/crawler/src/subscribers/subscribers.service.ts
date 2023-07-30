87abbe71eab15be3c4842d0cc6f63abad6fb7d0b
ashkuc <ashkucc@gmail.com>
2023-07-30 22:48:22 +0200

    added PgEventsListener service

--- apps/crawler/src/subscribers/subscribers.service.ts
+++ apps/crawler/src/subscribers/subscribers.service.ts
@@ -5,13 +5,9 @@ import { Config } from '../config/config.module';
 import { AccountsSubscriberService } from './accounts.subscriber.service';
 import { BlocksSubscriberService } from './blocks.subscriber.service';
 
-import { Client as PgClient } from 'pg';
-
 export interface ISubscriberService {
   subscribe();
 }
-
-export const FORCE_RESCAN_BLOCK = 'force_rescan_block';
 
 @Injectable()
 export class SubscribersService {
@@ -24,47 +20,6 @@ export class SubscribersService {
 
   private logger = new Logger(SubscribersService.name);
 
-  async listenPgEvents() {
-    const config = {
-      host: process.env.POSTGRES_HOST,
-      port: +process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-    };
-
-    const client = new PgClient(config);
-
-    client.on('notification', async ({ channel, payload }) => {
-      this.logger.log(`Received notification on channel ${channel}`);
-
-      if (channel === FORCE_RESCAN_BLOCK && payload) {
-        const blockNumbers = payload
-          .split(',')
-          .map((n) => parseInt(n.trim(), 10))
-          .filter((n) => !isNaN(n));
-
-        this.logger.log(
-          `Going to force rescan blocks: ${blockNumbers.join(', ')}`,
-        );
-
-        for (const blockNumber of blockNumbers) {
-          this.logger.log(`Rescan for block ${blockNumber}`);
-
-          await this.blocksSubscriberService.processBlockByNumber(blockNumber);
-
-          this.logger.log(`Rescan for block ${blockNumber} finished`);
-        }
-      }
-    });
-
-    await client.connect();
-    await client.query(`LISTEN ${FORCE_RESCAN_BLOCK}`);
-    this.logger.log(
-      `Listening for notifications on channel ${FORCE_RESCAN_BLOCK}`,
-    );
-  }
-
   async run() {
     const subscribersConfig = this.configService.get('subscribers');
 
@@ -73,8 +28,6 @@ export class SubscribersService {
         .subscribe()
         .then(() => this.logger.log('subscribe() resolved'));
     }
-
-    await this.listenPgEvents();
 
     //return this.processorService.run(this.configService.get('rescan'));
   }
